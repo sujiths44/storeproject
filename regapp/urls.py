217d7e96c51058db065881730821bbf5b6aee83b
sujith s <sujiths44@gmail.com>
2023-05-19 15:13:50 +0530

    storeproject added

---
+++ regapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+
+    path('',views.home,name='home'),
+    path('main',views.main,name='main'),
+    path('register',views.register, name='register'),
+    path('login',views.login,name='login'),
+    path('logout',views.logout,name='logout'),
+
+]
